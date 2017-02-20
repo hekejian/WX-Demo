a542292e157bbabc688ac3c10956eb5d22d5ec06
hekejian <fei.94625@qq.com>
2017-02-20 19:02:34 +0800

    mofify story css

--- pages/story/story.js
+++ pages/story/story.js
@@ -3,7 +3,7 @@ Page({
         groupInfo:null,
         story:[],
         imageList:[
-            "../materials/test1.jpg","../materials/test2.jpg"
+            "http://wx.qlogo.cn/mmopen/vi_32/TDj3GsR0VeYgXeC7JOJ0cHX0MmyTMu4kv843ZSJjo0XCUpT66aPlyydA5K7iaFbzRKmz3xLnxo2sEfdQ25KQp0g/0","http://wx.qlogo.cn/mmopen/vi_32/TDj3GsR0VeYgXeC7JOJ0cHX0MmyTMu4kv843ZSJjo0XCUpT66aPlyydA5K7iaFbzRKmz3xLnxo2sEfdQ25KQp0g/0","http://wx.qlogo.cn/mmopen/vi_32/TDj3GsR0VeYgXeC7JOJ0cHX0MmyTMu4kv843ZSJjo0XCUpT66aPlyydA5K7iaFbzRKmz3xLnxo2sEfdQ25KQp0g/0","http://wx.qlogo.cn/mmopen/vi_32/TDj3GsR0VeYgXeC7JOJ0cHX0MmyTMu4kv843ZSJjo0XCUpT66aPlyydA5K7iaFbzRKmz3xLnxo2sEfdQ25KQp0g/0","http://wx.qlogo.cn/mmopen/vi_32/TDj3GsR0VeYgXeC7JOJ0cHX0MmyTMu4kv843ZSJjo0XCUpT66aPlyydA5K7iaFbzRKmz3xLnxo2sEfdQ25KQp0g/0"
         ],
         title:"78路公交车",
         avatar:"http://wx.qlogo.cn/mmopen/vi_32/TDj3GsR0VeYgXeC7JOJ0cHX0MmyTMu4kv843ZSJjo0XCUpT66aPlyydA5K7iaFbzRKmz3xLnxo2sEfdQ25KQp0g/0"
@@ -18,11 +18,21 @@ Page({
    },
 
   previewImage: function (e) {
+    console.log(e.target.dataset.src)
+    console.log(this.data.imageList)
     var current = e.target.dataset.src
 
     wx.previewImage({
       current: current,
-      urls: this.data.imageList
+      urls: this.data.imageList,
+      success(args){
+          console.log("成功了")
+          console.log(args)
+      },
+      fail(){
+          console.log("失败了")
+          console.log(args)
+      }
     })
   }
 
